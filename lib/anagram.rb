2902e259bcfe0908a1f3f7ef3bf5fbe63551b63c
Yurina Shin <yurinas@gmail.com>
2018-01-06 20:40:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,8 @@ class Anagram
   end
 
   def match(word)
-    #word is array.
-    #array has to match with object @words.
   a = word.map{|c| c.split("")} # [[a b]]
+  binding.pry
   a.select{|c|c == @words.sort}
   # a = word.select{|c| c.split("").sort == @words}
   #
