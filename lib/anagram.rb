5e2763d237d324aeb2131c04c93144aacba500d4
Yurina Shin <yurinas@gmail.com>
2018-01-06 21:24:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :name
+
+  def initialize(word)
+    @words = word
+  end
+
+  def match(word)
+  # a = word.map{|c| c.split("").sort} # [[a b]]
+  # # binding.pry
+  # b = a.select{|c|c == @words.split("").sort}
+  # c = [b.join]
+  # c
+  word.select{|c| c.split("").sort == @words.split("").sort}
+  #
+  # a
+  end
+end# Your code goes here!
