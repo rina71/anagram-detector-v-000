5b745166e82000b640b8d57847b172c2e7b717b2
Yurina Shin <yurinas@gmail.com>
2018-01-06 21:23:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,12 +8,12 @@ class Anagram
   end
 
   def match(word)
-  a = word.map{|c| c.split("").sort} # [[a b]]
-  # binding.pry
-  b = a.select{|c|c == @words.split("").sort}
-  c = [b.join]
-  c
-  # a = word.select{|c| c.split("").sort == @words}
+  # a = word.map{|c| c.split("").sort} # [[a b]]
+  # # binding.pry
+  # b = a.select{|c|c == @words.split("").sort}
+  # c = [b.join]
+  # c
+  word.select{|c| c.split("").sort == @words.split("").sort}
   #
   # a
   end
