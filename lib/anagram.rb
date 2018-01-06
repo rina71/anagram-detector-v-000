89546ff55fe3f6327ad949db0db553db435e07c4
Yurina Shin <yurinas@gmail.com>
2018-01-06 20:46:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   a = word.map{|c| c.split("")} # [[a b]]
   binding.pry
   a.select{|c|c == @words.split("").sort}
-  
+
   # a = word.select{|c| c.split("").sort == @words}
   #
   # a
